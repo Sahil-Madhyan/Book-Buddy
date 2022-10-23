cf022d3ce8cda677ca7a80cd7cf6a9fa6a0c28ad
Sahil Madhyan <97780233+Sahil-Madhyan@users.noreply.github.com>
2022-10-23 15:22:02 +0530

    added login.html

--- script.js
+++ script.js
@@ -29,3 +29,16 @@ function signUp() {
   }
   
 }
+function login() {
+  var userName = document.getElementById("user").value;
+  var psswd = document.getElementById("psswd").value;
+  if (userName == "sahilmadhyan" && psswd == "sahil123") {
+    alert("Login Successful");
+    window.open(
+      "https://sahil-madhyan.github.io/Book-Buddy/",
+      "_self"
+    );
+  } else {
+    alert("Login Failed");
+  }
+}
