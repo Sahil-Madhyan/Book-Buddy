00da3519c252818997f376a682279caf6323dee3
Sahil Madhyan <sahilmadhyan880@gmail.com>
2023-01-13 19:57:56 +0530

    Updated files

--- Scripts/login.js
+++ Scripts/login.js
@@ -3,6 +3,7 @@ function login() {
   var psswd = document.getElementById("psswd").value;
   if (userName == "sahilmadhyan" && psswd == "sahil123") {
     alert("Login Successful");
+    document.getElementById('login').innerHTML = userName;
     window.open("https://sahil-madhyan.github.io/Book-Buddy/", "_self");
   } else {
     alert("Login Failed");
